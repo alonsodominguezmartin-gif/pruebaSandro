ffdafe767c43c23cc3bf31a21bab42cb44997a6c
alonsodominguezmartin-gif <alonsodominguezmartin@gmail.com>
2026-05-07 19:59:00 +0200

    Edicion para coonflicto

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Sandro, hola como estas");
       System.out.println("Ejercicio 1");
+       System.out.println("Ejercicio 1");
     }
 }
