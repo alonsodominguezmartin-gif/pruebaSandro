af41d5151a1f68ec60741dd43007339577144bdb
alonsodominguezmartin-gif <alonsodominguezmartin@gmail.com>
2026-05-07 19:55:08 +0200

    Edicion pagina principal

--- src/App.java
+++ src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Sandro, hola como estas");
       System.out.println("Ejercicio 1");
+     System.out.println("Ejercicio 5");
+    
     }
 }
