a3ffba3c64ed5cf771aa04584d4b66161b7323b8
alonsodominguezmartin-gif <alonsodominguezmartin@gmail.com>
2026-05-07 19:48:30 +0200

    Edicion rama develop

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Sandro, hola como estas");
-    
+      System.out.println("Ejercicio 1");
     }
 }
